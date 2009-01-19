004d11009fa75a5997cdf92ad447e9c6670d6c12
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-01-19 16:20:06 +0100

    Add a status option to tags

--- lib/from_date_tags.rb
+++ lib/from_date_tags.rb
@@ -8,12 +8,10 @@ module FromDateTags
     You can use siblings tags to process each page after or before this.
     
     *Usage:*
-    <pre><code><r:children:next_from_now>...</r:children:next_from_now></code></pre>
+    <pre><code><r:children:next_from_now [status="published"]>...</r:children:next_from_now></code></pre>
   }
   tag 'children:next_from_now' do |tag|
-    found = tag.locals.page.children.find(:first, 
-                  :conditions => ["published_at > ?", Time.now],
-                  :order => "published_at ASC")
+    found = tag.locals.page.children.find(:first, children_from_date_options(tag, "published_at ASC"))
     if page_found?(found)
       tag.locals.page = found
       tag.expand
@@ -25,12 +23,10 @@ module FromDateTags
     children page after now.
     
     *Usage:*
-    <pre><code><r:children:if_next_from_now>...</r:children:if_next_from_now></code></pre>
+    <pre><code><r:children:if_next_from_now [status="published"]>...</r:children:if_next_from_now></code></pre>
   }
   tag 'children:if_next_from_now' do |tag|    
-    found = tag.locals.page.children.find(:first, 
-                  :conditions => ["published_at > ?", Time.now],
-                  :order => "published_at ASC")
+    found = tag.locals.page.children.find(:first, children_from_date_options(tag, "published_at ASC"))
     if page_found?(found)
       tag.expand
     end
@@ -41,15 +37,32 @@ module FromDateTags
     children page after now.
     
     *Usage:*
-    <pre><code><r:children:unless_next_from_now>...</r:children:unless_next_from_now></code></pre>
+    <pre><code><r:children:unless_next_from_now [status="published"]>...</r:children:unless_next_from_now></code></pre>
   }
   tag 'children:unless_next_from_now' do |tag|    
-    found = tag.locals.page.children.find(:first, 
-                  :conditions => ["published_at > ?", Time.now],
-                  :order => "published_at ASC")
+    found = tag.locals.page.children.find(:first, children_from_date_options(tag, "published_at ASC"))
     if !page_found?(found)
       tag.expand
     end
+  end
+  
+  def children_from_date_options(tag, order)
+    attr = tag.attr.symbolize_keys
+    
+    options = {}
+    options[:order] = order 
+    status = (attr[:status] || 'published').downcase
+    unless status == 'all'
+      stat = Status[status]
+      unless stat.nil?
+        options[:conditions] = ["(virtual = ?) and (status_id = ?) and (published_at > ?)", false, stat.id, Time.now]
+      else
+        raise TagError.new(%{`status' attribute of `next_from_now' tag must be set to a valid status})
+      end
+    else
+      options[:conditions] = ["(virtual = ?) and (published_at > ?)", false, Time.now]
+    end
+    options
   end
   
   # TODO:
@@ -60,7 +73,6 @@ module FromDateTags
   # 'next_from_date'
   # 'if_next_from_date'
   # 'unless_next_from_date'
-  # 
   # 'previous_from_date'
   # 'if_previous_from_date'
   # 'unless_previous_from_date'
